8447ddb5868c5f37b81a4e8a424aeb5feb18357a
unknown <durgabhavanid83@gmail.com>
2024-02-28 12:50:30 +0530

    table

---
+++ src/components/function-components/12-feb/table1.js
@@ -0,0 +1,54 @@
+
+function Tablecomponent(){
+
+    return(
+        <div>
+        <table> 
+            <tr>
+            <th>id</th>
+            <th>product name</th>
+            <th>price</th>
+            <th>brand</th>
+            <th>image</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>i phone</td>
+                <td>20000</td>
+                <td>iphone 11</td>
+                <td><img src="https://tse2.mm.bing.net/th?id=OIP.lxETeYrc9HMOm4tChiNopgHaJx&pid=Api&P=0&h=180" height={100} width={100}></img></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>laptop</td>
+                <td>50000</td>
+                <td>hp</td>
+                <td><img src="https://tse3.mm.bing.net/th?id=OIP.is1B9OoMc7ZrbAfWN4QJ9wHaGp&pid=Api&P=0&h=180"height={100} width={100}></img></td>
+
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>tv</td>
+                <td>70000</td>
+                <td>samsung</td>
+                <td><img src="https://tse1.mm.bing.net/th?id=OIP.t6FQsL3ntKtlIvQK1VwhfgHaHa&pid=Api&P=0&h=180" height={100} width={100}></img></td>
+
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>smart watch</td>
+                <td>15000</td>
+                <td>samsung</td>
+                <td><img src="https://tse2.mm.bing.net/th?id=OIP.ip3oXNMOb1ZE1s-3jOzHBQHaHa&pid=Api&P=0&h=180" height={100} width={100}></img></td>
+                
+            </tr>
+            
+        
+            
+        </table>
+        </div>
+
+    )
+}
+
+export default Tablecomponent;
